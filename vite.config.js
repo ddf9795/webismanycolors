4c15a21e65d6706e647f259dc4ba475e83e81126
Dean <89398656+ddf9795@users.noreply.github.com>
2024-03-08 14:44:14 -0500

    port repo

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: './src',
+  test: {
+    exclude: ['**/e2e/**'],
+    root: './',
+    reporters: ['default'],
+  },
+})
